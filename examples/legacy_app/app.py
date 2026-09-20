b9756f57d7b5ea63916efc4ea2626160e3052e6b
Achir othmane <othy19904@gmail.com>
2026-09-20 01:41:51 +0100

    feat: add modernization factory MVP

---
+++ examples/legacy_app/app.py
@@ -0,0 +1,16 @@
+import imp
+from collections import MutableMapping
+
+
+def load_plugin(name):
+    return imp.load_source(name, name + ".py")
+
+
+class Config(MutableMapping):
+    def __init__(self):
+        self.data = {}
+    def __getitem__(self, key): return self.data[key]
+    def __setitem__(self, key, value): self.data[key] = value
+    def __delitem__(self, key): del self.data[key]
+    def __iter__(self): return iter(self.data)
+    def __len__(self): return len(self.data)
